54eb93106c265dcbfa569511b65d9b02cbbaf8c7
onz sbz <onz.sbz@gmail.com>
2025-09-22 22:57:46 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type {NextConfig} from 'next';
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -35,11 +28,15 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      }
     ],
     unoptimized: true,
   },
 };
 
-export default withPWA(nextConfig);
-
-    
+export default nextConfig;
