50c1e6d412d58a12ee825fbdf0b955d457e6edde
onz sbz <onz.sbz@gmail.com>
2025-09-23 16:05:28 +0000

    ok guide moi pour finaliser ce projet et le pousser sur github ensuite l

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://gqfcfzlwadqpgiqlkggp.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZmNmemx3YWRxcGdpcWxrZ2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxMDM4NDgsImV4cCI6MjAzNTY3OTg0OH0.jUq2_5q5fLdYQ3n5jN2N33Y_692-l_2Kj9fTf_ATtMc"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
