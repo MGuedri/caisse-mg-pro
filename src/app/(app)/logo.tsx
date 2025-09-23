046a4db90b1046c389d1f108980193232d1caeef
onz sbz <onz.sbz@gmail.com>
2025-09-23 08:04:51 +0000

    ajouter mon, logo dans le cercle creer pour le logo actuel

--- src/app/(app)/logo.tsx
+++ src/app/(app)/logo.tsx
@@ -13,24 +13,41 @@ export const Logo = ({ size = 'lg', className }: { size?: 'sm' | 'lg', className
         "relative flex items-center justify-center bg-transparent rounded-full border-white",
         isLarge ? 'w-24 h-24 border-4' : 'w-10 h-10 border-2'
       )}>
-        <span className={cn(
-          "font-black text-primary", // Orange M
-          isLarge ? 'text-5xl' : 'text-xl'
-        )}>
-          M
-        </span>
-        <span className={cn(
-          "font-black text-blue-500 absolute", // Blue G
-          isLarge ? 'text-5xl left-[48%]' : 'text-xl left-[48%]'
-        )}>
-          G
-        </span>
+        <svg
+          viewBox="0 0 100 50"
+          className={cn(isLarge ? 'w-20 h-10' : 'w-8 h-4')}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
+              <feOffset dx="2" dy="2" in="SourceAlpha" result="offset" />
+              <feGaussianBlur in="offset" stdDeviation="0" result="blur" />
+              <feFlood floodColor="hsl(var(--chart-2))" result="color" />
+              <feComposite in="color" in2="blur" operator="in" result="shadow" />
+              <feComponentTransfer in="shadow" result="shadow">
+                <feFuncA type="linear" slope="1" />
+              </feComponentTransfer>
+              <feMerge>
+                <feMergeNode in="shadow" />
+                <feMergeNode in="SourceGraphic" />
+              </feMerge>
+            </filter>
+          </defs>
+          <text
+            x="50%"
+            y="50%"
+            dominantBaseline="middle"
+            textAnchor="middle"
+            fill="white"
+            fontSize="48"
+            fontFamily="PT Sans, sans-serif"
+            fontWeight="900"
+            filter="url(#shadow)"
+          >
+            MG
+          </text>
+        </svg>
       </div>
-      {isLarge && (
-        <span className="text-xl text-white tracking-widest font-light">
-          Café Mon Plaisir
-        </span>
-      )}
     </div>
   );
 };
