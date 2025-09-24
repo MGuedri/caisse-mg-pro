80854cc6815a92f2a9236c50f185128a049be1dd
onz sbz <onz.sbz@gmail.com>
2025-09-24 07:48:05 +0000

    Pro

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-
-'use server';
-// This page is now a simple entry point to the main app's login screen.
-// We redirect to the / route, and the layout will handle showing the login screen if not authenticated.
-import { redirect } from "next/navigation";
-
-export default async function LoginPage() {
-    redirect('/');
-}
